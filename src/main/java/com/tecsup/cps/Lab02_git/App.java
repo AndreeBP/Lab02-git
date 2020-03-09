52af93c1a1cf699e382df7247c51527554ef10de
Alumno <Alumno@LAB1504-19>
2020-03-09 16:50:53 -0500

    Primera version

---
+++ src/main/java/com/tecsup/cps/Lab02_git/App.java
@@ -0,0 +1,13 @@
+package com.tecsup.cps.Lab02_git;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
