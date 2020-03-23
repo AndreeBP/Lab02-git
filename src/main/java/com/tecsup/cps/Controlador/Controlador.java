9823cb46c762b143d17307361bed676d3f0ae805
gianluca.gave@tecsup.edu.pe <gianluca.gave@tecsup.edu.pe>
2020-03-23 17:16:19 -0500

    Mi primer upload

---
+++ src/main/java/com/tecsup/cps/Controlador/Controlador.java
@@ -0,0 +1,10 @@
+package com.tecsup.cps.Controlador;
+
+
+public class Controlador {
+	public static void main( String[] args )
+    {
+    	String name = "Luis Angel";
+        System.out.println( name );
+    }
+}
---
+++ src/main/java/com/tecsup/cps/Model/Model.java
@@ -0,0 +1,10 @@
+package com.tecsup.cps.Model;
+
+
+public class Model {
+	public static void main( String[] args )
+    {
+    	String name = "Luis Angel";
+        System.out.println( name );
+    }
+}
