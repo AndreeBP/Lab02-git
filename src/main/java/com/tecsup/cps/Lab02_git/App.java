c5131e722c35646071ec34cf35af9d64c17b3618
andree <PROPIETARIO@LENOVO.com>
2020-03-23 15:59:37 -0500

    Nuevo comentario

--- src/main/java/com/tecsup/cps/Lab02_git/App.java
+++ src/main/java/com/tecsup/cps/Lab02_git/App.java
@@ -8,8 +8,11 @@ public class App
 {
     public static void main( String[] args )
     {
-    	String name = "Luis Angel";
-        System.out.println( name );
+        System.out.println("Hello World!" );
+    }
+    public static void mensaje(){
+        System.out.println("Mensaje");
     }
     
+//TO DO
 }
